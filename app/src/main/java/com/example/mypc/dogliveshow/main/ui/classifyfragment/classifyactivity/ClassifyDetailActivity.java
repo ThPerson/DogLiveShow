4bd0a10e53d7e26af2bf084b49572bc11ace8177
oschinawwt <wwt923277>
2016-08-13 08:46:31 +0800

    第三次提交代码, 栏目分类模块:下拉刷新

---
+++ app/src/main/java/com/example/mypc/dogliveshow/main/ui/classifyfragment/classifyactivity/ClassifyDetailActivity.java
@@ -0,0 +1,15 @@
+package com.example.mypc.dogliveshow.main.ui.classifyfragment.classifyactivity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.mypc.dogliveshow.R;
+
+public class ClassifyDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_classify_detail);
+    }
+}
